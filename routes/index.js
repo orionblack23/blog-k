01ee228151ca7a328df50c04ce84dc4b96fc883c
ORION BLACK <orionb.y23@gmail.com>
2019-06-28 14:01:42 -0700

    importing blog template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {  });
+});
+
+/* GET about page */
+router.get('/about', function(req, res, next) {
+  res.render('about', {});
+});
+
+/* GET contact page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {});
+});
+
+/* GET post page */
+router.get('/post', function(req, res, next) {
+  res.render('post', {});
 });
 
 module.exports = router;
